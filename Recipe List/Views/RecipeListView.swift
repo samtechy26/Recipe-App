e337909b8a6d408125fb55ace229fa2fb0d125ff
samtechy26 <samkan061@gmail.com>
2022-03-10 06:10:05 -0500

    modified the recipe list view to be consistent with the featured recipe view

--- Recipe List/Views/RecipeListView.swift
+++ Recipe List/Views/RecipeListView.swift
@@ -10,28 +10,43 @@ import SwiftUI
 struct RecipeListView: View {
     @EnvironmentObject var model:RecipeModel
     var body: some View {
+        
         NavigationView{
-            List(model.recipes){
-             r in
-                NavigationLink(destination: RecipeDetail(recipe: r), label: {
-                    HStack(spacing: 20.0){
-                        Image(r.image).resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment:.center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
+            VStack(alignment: .leading){
+                Text("All Recipes").bold()
+                    
+                    .padding(.top,40)
+                
+                    .font(.largeTitle)
+                ScrollView{
+                    LazyVStack(alignment: .leading){
+                        
+                        ForEach(model.recipes){
+                            r in
+                            NavigationLink(destination: RecipeDetail(recipe: r), label: {
+                                HStack(spacing: 20.0){
+                                    Image(r.image).resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment:.center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name).foregroundColor(.black)
+                                }
+                            })
+                            
+                        }
                     }
-                })
+                }
+                
+            }.padding(.leading)
             
-        }
-            .navigationBarTitle("All Recipes")
+            .navigationBarHidden(true)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        RecipeListView().environmentObject(RecipeModel())
     }
 }
